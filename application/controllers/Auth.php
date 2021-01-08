d4d6185c773edff08113fe5373dfc29e2d7e7e09
NahrulK <khayattullah73@gmail.com>
2021-01-08 16:57:06 +0800

    Edit Profil #10 finish

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -13,6 +13,10 @@ class Auth extends CI_Controller
     public function index()
     {
 
+        if ($this->session->userdata("email")) {
+            redirect('user');
+        }
+
         $this->form_validation->set_rules('email', 'Email', 'trim|required|valid_email');
         $this->form_validation->set_rules('password', 'Password', 'trim|required');
 
@@ -77,6 +81,10 @@ class Auth extends CI_Controller
     public function registration()
     {
 
+        if ($this->session->userdata("email")) {
+            redirect('user');
+        }
+
         $this->form_validation->set_rules('name', 'Name', 'required|trim');
         $this->form_validation->set_rules('email', 'Email', 'required|trim|valid_email|is_unique[user.email]', [
             'is_unique' => "Email ini sudah digunakan"
@@ -121,10 +129,5 @@ class Auth extends CI_Controller
 
         $this->session->set_flashdata('message', '<div class="alert alert-success" role="alert">Berhasil Logout.!</div>');
         redirect('auth');
-    }
-
-    public function blocked()
-    {
-        $this->load->view('auth/blocked');
     }
 }
--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -61,10 +61,10 @@
  </script>
 
  <script>
-     //  $('.custom-file-input').on('change', function() {
-     //      let fileName = $(this).val().split('\\').pop();
-     //      $(this).next('.custom-file-label').addClass("selected").html(fileName);
-     //  });
+     $('.custom-file-input').on('change', function() {
+         let fileName = $(this).val().split('\\').pop();
+         $(this).next('.custom-file-label').addClass("selected").html(fileName);
+     });
 
      $('.form-check-input').on('click', function() {
          const menuId = $(this).data('menu');
