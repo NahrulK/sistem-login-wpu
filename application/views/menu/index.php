5a841404b724b53286e295516c4d230820d4740d
NahrulK <khayattullah73@gmail.com>
2021-01-08 10:34:28 +0800

    Sub Menu Management #7 finish

---
+++ application/views/menu/index.php
@@ -0,0 +1,76 @@
+<!-- Begin Page Content -->
+<div class="container-fluid">
+
+    <!-- Page Heading -->
+    <h1 class="h3 mb-4 text-gray-800"><?php echo $title; ?></h1>
+
+
+
+    <div class="row">
+        <div class="col-lg-6">
+
+            <?php echo form_error('menu', '<div class="alert alert-danger" role="alert">', '</div>'); ?>
+
+            <?php echo $this->session->flashdata('message'); ?>
+
+            <a href="" class="btn btn-primary mb-3" data-toggle="modal" data-target="#newMenuModal">Tambah Menu Baru</a>
+
+            <table class="table table-hover">
+                <thead>
+                    <tr>
+                        <th scope="col">Index</th>
+                        <th scope="col">Menu</th>
+                        <th scope="col">Aksi</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php $i = 1; ?>
+                    <?php foreach ($menu as $m) : ?>
+                        <tr>
+                            <th scope="row"><?= $i; ?></th>
+                            <td><?php echo $m['menu']; ?></td>
+                            <td>
+                                <a href="" class="badge badge-pill badge-success">Edit</a>
+                                <a href="" class="badge badge-pill badge-danger">Hapus</a>
+                            </td>
+                        </tr>
+                        <?php $i++; ?>
+                    <?php endforeach ?>
+                </tbody>
+            </table>
+
+        </div>
+    </div>
+
+
+</div>
+<!-- /.container-fluid -->
+
+</div>
+<!-- End of Main Content -->
+
+<!-- Modal -->
+<div class="modal fade" id="newMenuModal" tabindex="-1" aria-labelledby="newMenuModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="newMenuModalLabel">Tamba Menu Baru</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <form action="<?= base_url('menu'); ?>" method="post">
+                <div class="modal-body">
+                    <div class="form-group">
+                        <input type="text" class="form-control" id="menu" name="menu" placeholder="Nama Menu Baru">
+                    </div>
+                </div>
+
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                    <button type="submit" class="btn btn-primary">Tambah</button>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
